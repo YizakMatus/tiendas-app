645b7d71b0b79ddd95a5af20e47245351c7ab2d5
Yizak Matus <feint01@gmail.com>
2020-07-06 14:16:38 -0500

    agrega método para obtener todos los comercios

---
+++ tiendas-app-backend/src/main/java/tiendasapp/controller/CommerceController.java
@@ -0,0 +1,43 @@
+package tiendasapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+import tiendasapp.entity.Commerce;
+import tiendasapp.repository.CommerceRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("commerce")
+public class CommerceController {
+
+    private final CommerceRepository commerceRepository;
+
+    public CommerceController(CommerceRepository commerceRepository) {
+        this.commerceRepository = commerceRepository;
+    }
+
+    @PostMapping(path = "/add")
+    public @ResponseBody
+    String addNewCommerce(@RequestParam String name,
+                          @RequestParam String ruc,
+                          @RequestParam String email,
+                          @RequestParam String address,
+                          @RequestParam String phone,
+                          @RequestParam String password) {
+        Commerce commerce = new Commerce();
+        commerce.setName(name);
+        commerce.setRuc(ruc);
+        commerce.setEmail(email);
+        commerce.setAddress(address);
+        commerce.setPhone(phone);
+        commerce.setPassword(password);
+        commerceRepository.save(commerce);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/")
+    public @ResponseBody
+    List<Commerce> getAllCommerces() {
+        return (List<Commerce>) commerceRepository.findAll();
+    }
+}
