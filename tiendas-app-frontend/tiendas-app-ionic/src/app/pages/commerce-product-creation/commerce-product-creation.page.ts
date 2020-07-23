7a18f49a4172e43214a0c2a0d76a2e38eaccf63e
Yizak Matus <feint01@gmail.com>
2020-07-22 21:22:52 -0500

    añade formulario de registro de producto

--- tiendas-app-frontend/tiendas-app-ionic/src/app/pages/commerce-product-creation/commerce-product-creation.page.ts
+++ tiendas-app-frontend/tiendas-app-ionic/src/app/pages/commerce-product-creation/commerce-product-creation.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-commerce-product-creation',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commerce-product-creation.page.scss'],
 })
 export class CommerceProductCreationPage implements OnInit {
+  product = {}
+  submitted = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSave(form: NgForm){
+    this.submitted = true;
+
+    if (form.valid) {
+      console.log(form.value);
+      // this.userData.login(this.login.username);
+      // this.router.navigateByUrl('/commerce-products');
+    }
   }
 
 }
