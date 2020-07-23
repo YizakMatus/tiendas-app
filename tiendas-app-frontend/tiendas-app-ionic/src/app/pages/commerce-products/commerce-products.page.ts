5885db4b01d2098539639eafe887eb0152dde7db
Yizak Matus <feint01@gmail.com>
2020-07-22 21:32:35 -0500

    añade funcionalidad al botón de agregar producto

--- tiendas-app-frontend/tiendas-app-ionic/src/app/pages/commerce-products/commerce-products.page.ts
+++ tiendas-app-frontend/tiendas-app-ionic/src/app/pages/commerce-products/commerce-products.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../providers/products/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-commerce-products',
@@ -9,11 +10,15 @@ import { ProductsService } from '../../providers/products/products.service';
 export class CommerceProductsPage implements OnInit {
   products: [];
 
-  constructor(private productsService: ProductsService) { }
+  constructor(private productsService: ProductsService, private router: Router) { }
 
   ngOnInit() {
     this.productsService.load().subscribe(data => {
       this.products = data;
     })
+  }
+
+  onAddProduct() {
+    this.router.navigateByUrl('/commerce-product-creation');
   }
 }
