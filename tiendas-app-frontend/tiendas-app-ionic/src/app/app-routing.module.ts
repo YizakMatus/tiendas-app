95713f1dad17cf85aa5dcfa2f786dd381d5d9cf4
Yizak Matus <feint01@gmail.com>
2020-07-25 14:25:45 -0500

    mueve servicio y página a sus directorios correspondientes

--- tiendas-app-frontend/tiendas-app-ionic/src/app/app-routing.module.ts
+++ tiendas-app-frontend/tiendas-app-ionic/src/app/app-routing.module.ts
@@ -67,7 +67,7 @@ const routes: Routes = [
   },
   {
     path: 'shopping-cart',
-    loadChildren: () => import('./shopping-cart/shopping-cart.module').then( m => m.ShoppingCartPageModule)
+    loadChildren: () => import('./pages/shopping-cart/shopping-cart.module').then( m => m.ShoppingCartPageModule)
   }
 ];
 
