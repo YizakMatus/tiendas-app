3f4c8e9ec54e53d7143a23849b71fff49c272491
Yizak Matus <feint01@gmail.com>
2020-07-25 14:29:49 -0500

    personaliza página de lista de compras

--- tiendas-app-frontend/tiendas-app-ionic/src/app/pages/shopping-cart/shopping-cart.page.ts
+++ tiendas-app-frontend/tiendas-app-ionic/src/app/pages/shopping-cart/shopping-cart.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ShoppingCartService } from "../../providers/shopping-cart/shopping-cart.service";
 
 @Component({
-  selector: 'app-shopping-cart',
-  templateUrl: './shopping-cart.page.html',
-  styleUrls: ['./shopping-cart.page.scss'],
+  selector: "app-shopping-cart",
+  templateUrl: "./shopping-cart.page.html",
+  styleUrls: ["./shopping-cart.page.scss"],
 })
 export class ShoppingCartPage implements OnInit {
+  products = [];
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewWillEnter() {
+    this.products = this.shoppingCartService.products;
   }
-
 }
