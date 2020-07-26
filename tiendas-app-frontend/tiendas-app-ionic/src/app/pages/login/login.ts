ac106b611def4daf7e9f378be980b41a58f642ac
Yizak Matus <feint01@gmail.com>
2020-07-26 12:59:48 -0500

    arregla formulario de autenticación

--- tiendas-app-frontend/tiendas-app-ionic/src/app/pages/login/login.ts
+++ tiendas-app-frontend/tiendas-app-ionic/src/app/pages/login/login.ts
@@ -1,33 +1,43 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 
-import { UserData } from '../../providers/user-data';
+import { UserData } from "../../providers/user-data";
 
-import { UserOptions } from '../../interfaces/user-options';
-
-
+import { UserOptions } from "../../interfaces/user-options";
+import { AuthService } from "../../providers/auth/auth.service";
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
-  styleUrls: ['./login.scss'],
+  selector: "page-login",
+  templateUrl: "login.html",
+  styleUrls: ["./login.scss"],
 })
 export class LoginPage {
-  login: UserOptions = { username: '', password: '' };
+  login = {};
   submitted = false;
 
   constructor(
     public userData: UserData,
-    public router: Router
-  ) { }
+    public router: Router,
+    private authService: AuthService
+  ) {}
 
   onLogin(form: NgForm) {
     this.submitted = true;
 
     if (form.valid) {
-      this.userData.login(this.login.username);
-      this.router.navigateByUrl('/commerce-products');
+      console.log(form.value);
+      this.authService
+        .authenticate(form.value.email, form.value.password)
+        .subscribe(
+          (user) => {
+            console.log(user);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      // this.router.navigateByUrl("/commerce-products");
     }
   }
 }
