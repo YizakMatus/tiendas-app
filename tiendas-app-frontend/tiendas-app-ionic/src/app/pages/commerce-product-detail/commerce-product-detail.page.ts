105803b0b958d0b999537ef03a6a256a2312f053
Yizak Matus <feint01@gmail.com>
2020-07-27 20:05:05 -0500

    corrige detalles de productos

--- tiendas-app-frontend/tiendas-app-ionic/src/app/pages/commerce-product-detail/commerce-product-detail.page.ts
+++ tiendas-app-frontend/tiendas-app-ionic/src/app/pages/commerce-product-detail/commerce-product-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ProductsService } from "../../providers/products/products.service";
 import { RouteConfigLoadStart, ActivatedRoute } from "@angular/router";
+import { AuthService } from "../../providers/auth/auth.service";
 
 @Component({
   selector: "app-commerce-product-detail",
@@ -12,16 +13,19 @@ export class CommerceProductDetailPage implements OnInit {
 
   constructor(
     private productsService: ProductsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {}
 
   ionViewWillEnter() {
-    this.productsService.load().subscribe((data) => {
-      const index = this.route.snapshot.paramMap.get("index");
-      console.log(data[index]);
-      this.product = data[index];
-    });
+    this.productsService
+      .getProductsByCommerce(this.authService.user.id)
+      .subscribe((data) => {
+        const index = this.route.snapshot.paramMap.get("index");
+        console.log(data[index]);
+        this.product = data[index];
+      });
   }
 }
